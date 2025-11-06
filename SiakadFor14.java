e177bd06e9e3d104e475e8a5a4975b4241af8094
onlyyourza <paseoking456@gmail.com>
2025-11-06 15:22:56 +0700

    add pass and fail student count

--- SiakadFor14.java
+++ SiakadFor14.java
@@ -5,21 +5,32 @@ public class SiakadFor14 {
         double grade;
         double highest = 0;
         double lowest = 100;
+        int passCount = 0;
+        int failCount = 0;
 
-        for (int i = 1; i <=10; i++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.print("Enter the grade of the " + i + " student: ");
             grade = sc.nextDouble();
 
             if (grade > highest) {
                 highest = grade;
             }
-
             if (grade < lowest) {
                 lowest = grade;
             }
+
+            if (grade >= 60) {
+                passCount++;
+            } else {
+                failCount++;
+            }
         }
-        System.out.println("The highest grade is: " + highest);
+
+        System.out.println("\nThe highest grade is: " + highest);
         System.out.println("The lowest grade is: " + lowest);
+        System.out.println("Number of students who passed: " + passCount);
+        System.out.println("Number of students who failed: " + failCount);
+
         sc.close();
     }
 }
